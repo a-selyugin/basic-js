cf30ab0d090bdbd9002143ed6f6276742228e5a9
a-selyugin <selyuginai@gmail.com>
2022-05-01 11:08:28 +0300

    feat: extended-repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str,options) {
+  let ans = '';
+  let tempStr = '';
+  let addition = '';
+  let tmpAddition = '';
+  if (!(str === undefined)){
+
+    tempStr = String(str);
+    ans = tempStr;
+
+    if (!(options.addition === undefined)){
+      tmpAddition = String(options.addition);
+      addition = String(options.addition);
+
+      if (options.additionRepeatTimes){
+
+        if (!(options.additionSeparator === undefined)){
+          tmpAddition = String(options.additionSeparator) + tmpAddition; 
+        }
+        else{
+          tmpAddition = '|' + tmpAddition;
+        }
+        for (let n = 0; n < (options.additionRepeatTimes - 1); n++){
+          addition += tmpAddition;
+        }
+      }
+
+      tempStr += addition;
+      ans += addition;
+
+    }
+
+    if (options.repeatTimes){
+
+      if (!(options.separator === undefined)){
+        tempStr = String(options.separator) + tempStr;
+      }
+      else {
+        tempStr = '+' + tempStr;
+      }
+
+      for (let i = 0; i < (options.repeatTimes - 1); i++){
+        ans += tempStr;
+      }
+    }
+    return ans;
+  }
 }
 
 module.exports = {
